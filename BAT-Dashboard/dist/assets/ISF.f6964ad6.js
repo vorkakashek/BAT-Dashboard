f38e2d41acf55e35b576e54497d791a4f0734650
Vyacheslav Lutsenko <84517362+SlaweekLut@users.noreply.github.com>
2024-03-15 20:57:30 +0500

    feat: new build

---
+++ BAT-Dashboard/dist/assets/ISF.f6964ad6.js
@@ -0,0 +1,1 @@
+import{N as a}from"./NavigationTiles.951ad02a.js";import{r,o as e,c as s,g as n,h as c,b as m,F as p}from"./index.f9852411.js";const f={setup(l){return(t,i)=>{const o=r("RouterView");return e(),s(p,null,[t.$route.path.split("/").at(-1)==="ISF"?(e(),n(a,{key:0})):c("",!0),m(o)],64)}}};export{f as default};
